e9d7f959017a9e7b6c78ed5617a08beb06adec33
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-31 23:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 
   return `Welcome, ${name}. You are number ` + katzDeliLine.length + ` in line.`;
+}
+
+function nowServing() {
+
+  if ()
 }
